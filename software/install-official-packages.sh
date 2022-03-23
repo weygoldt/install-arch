5292d825e8a88dff8626c575d116eb93ade05696
weygoldt <88969563+weygoldt@users.noreply.github.com>
2022-03-23 09:26:00 +0100

    added cronie service start

--- software/install-official-packages.sh
+++ software/install-official-packages.sh
@@ -4,6 +4,14 @@ cd
 # software
 sudo pacman -S --noconfirm torbrowser-launcher torsocks keepassxc veracrypt libreoffice texlive-most biber pandoc firefox inkscape qbittorrent vlc ufw zsh neofetch tk r youtube-dl conky filelight cronie
 
-# software configuration
+
+
+# Enable firewall (did not execute last time?)
 sudo ufw enable
+
+# Change default shell to zsh (also did not execute for some reason)
 sudo chsh -s /usr/bin/zsh
+
+# Enable cronie so that scheduled timeshift snapshots are actually executed
+systemctl enable cronie.service 
+systemctl start cronie.service 
