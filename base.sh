9d42377e72460650d2e4f0b48ddacd35ac2798ec
weygoldt <88969563+weygoldt@users.noreply.github.com>
2022-03-23 21:07:12 +0100

    added linux-lts to readme, added nvidia-lts to base.sh

--- base.sh
+++ base.sh
@@ -25,7 +25,7 @@ echo root:password | chpasswd
 pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset sof-firmware nss-mdns acpid os-prober ntfs-3g intel-ucode
 
 # Install gpu driver
-pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+pacman -S --noconfirm nvidia nvidia-lts nvidia-utils nvidia-settings
 
 # Installs grub on efi parition. Change the directory to /boot/efi if you mounted the EFI partition at /boot/efi
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB 
