2796bf22978e0b5553abc5657fbe9a1168de7346
weygoldt <88969563+weygoldt@users.noreply.github.com>
2022-03-24 19:05:30 +0100

    fixed dotfiles script

--- software/dotfiles.sh
+++ software/dotfiles.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+# A script to install my most used dotfiles on a new linux system.
 
-# clone dotfiles 
-cd
-echo 'alias dfs="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"' >> $HOME/.zshrc
+cd # change dir to home
+
+# append alias to .zshrc. Remove comment for new .zshrc
+# echo 'alias dfs="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"' >> $HOME/.zshrc
+
+# source new .zshrc
 source ~/.zshrc
-echo ".dotfiles.git" >> .gitignore
-git clone --bare https://www.github.com/weygoldt/repo.git $HOME/.dotfiles
+
+# add dotfiles repo to gitignore
+echo ".dotfiles" >> .gitignore
+
+# clone bare repository to home
+git clone --bare https://github.com/weygoldt/dotfiles.git $HOME/.dotfiles
+
+# create dotfiles in home dir
 dfs checkout
+
+# hide untracked files warnings 
 dfs config --local status.showUntrackedFiles no
