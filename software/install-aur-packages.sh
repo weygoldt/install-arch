e98c7bad0d257c4b07214f746e34147fa2828e04
weygoldt <88969563+weygoldt@users.noreply.github.com>
2022-03-24 17:08:20 +0100

    formatted and commented package list for aur

--- software/install-aur-packages.sh
+++ software/install-aur-packages.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
-cd
+cd # change dir to home
 
-paru -S zsh-autosuggestions-git zsh-completions-git zsh-syntax-highlighting-git rstudio-desktop visual-studio-code-bin espanso-git 1password timeshift timeshift-autosnap optimus-manager optimus-manager-qt zramd
+PACKAGES=(
+    zsh-autosuggestions-git         # for autosuggestions (as in manjaro)
+    zsh-completions-git             # not working yet
+    zsh-syntax-highlighting-git     # for syntax highlighting
+    rstudio-desktop                 # IDE for R
+    visual-studio-code-bin          # text editor for the lazy
+    espanso-git                     # espanso unstable from git
+    1password                       # password manager for web
+    timeshift                       # for snapshots
+    timeshift-autosnap              # for autosnaps before system upgrades
+    optimus-manager                 # to manage hybrid graphics
+    optimus-manager-qt              # a frontend for qt based desktop environments
+    zramd                           # to enable zram
+)
+
+paru -S ${PACKAGES[@]}
 
 # enable zramd service
 sudo systemctl enable --now zramd.service
